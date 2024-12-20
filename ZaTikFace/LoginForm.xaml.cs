9d580c68ec2604c7ddebb83de3b20f0bcb77b2f2
Trinhexe <lekhanhtrinht9@gmail.com>
2024-12-20 14:36:17 +0700

    up

---
+++ ZaTikFace/LoginForm.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ZaTikFace
+{
+
+    public partial class LoginForm : Window
+    {
+        public static SqlConnection connection;
+        public LoginForm()
+        {
+            InitializeComponent();
+            connection = MainClass.GetConnection();
+        }
+
+        private void txtSdt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSDT.Focus();
+        }
+
+        private void txtSDT_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Kiểm tra nếu TextBox có dữ liệu thì ẩn TextBlock
+            txbSdt.Visibility = string.IsNullOrEmpty(txtSDT.Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void txbPass_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPass.Focus();
+        }
+
+        private void txtPass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            txbPass.Visibility = string.IsNullOrEmpty(txtPass.Password) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát", "Thông báo", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            if (txtSDT.Text == "" || txtPass.Password == "")
+            {
+
+                MessageBox.Show("Vui lòng nhập tài khoản và mật khẩu", "Thông báo", MessageBoxButton.OK);
+                return;
+            }
+            if (MainClass.DangNhap(txtSDT.Text, txtPass.Password.ToString()))
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButton.OK);
+                TrangChu trangChu = new TrangChu();
+                this.Hide();
+                trangChu.ShowDialog();
+
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng", "Thông báo", MessageBoxButton.OK);
+                txtSDT.Focus();
+            }
+        }
+    }
+}
---
+++ ZaTikFace/MainClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ZaTikFace
+{
+    public static class MainClass
+    {
+        static string KN = "Data Source=trinhlk;Initial Catalog=Login;Integrated Security=True;Encrypt=False";
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(KN);
+
+        }
+        public static bool DangNhap(string tk, string mk)
+        {
+            bool ktr = false;
+            string sql = @"Select UserName,Password from TaiKhoan
+                        Where (UserName ='"+tk+"' and Password ='"+mk+"')";
+            SqlCommand cmd = new SqlCommand(sql, MainClass.GetConnection());
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                ktr = true;
+            }
+            return ktr;
+        }
+    }
+}
